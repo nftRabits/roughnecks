f338d49564ab34196b016b65d5c9875f44ca9204
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-28 20:43:26 +0500

    bug fixed

--- views/Roadmap.js
+++ views/Roadmap.js
@@ -6,7 +6,7 @@ const Roadmap = () => {
       <div className="pt-20">
         <MainTitle title="ROAD MAP" />
       </div>
-      <div className="roadmap-bg">
+      <div className="roadmap-bg overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 py-20">
           <div className="timeline">
             <div className="container left">
