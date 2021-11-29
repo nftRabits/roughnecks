dbc7737262e43adb909c37174473ac489ab43e0f
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-29 17:31:45 +0500

    video added and video thumbnail also updated

--- views/Video.js
+++ views/Video.js
@@ -1,12 +1,23 @@
 import React from "react";
+import ReactPlayer from 'react-player'
 
 const Video = () => {
   return (
     <div>
       <div className="w-ful bg-primary relative">
-        <video preload={'auto'} controls={true} autoPlay={false} loop={true} muted={false} id="myVideo">
-          <source src="/images/Roughneck Origin.mp4" type="video/mp4" />
-        </video>
+        {/*<video preload={'auto'} controls={true} autoPlay={false} loop={true} muted={false} id="myVideo">*/}
+        {/*  <source src="/images/Roughneck Origin.mp4" type="video/mp4" />*/}
+        {/*</video>*/}
+          <div className='player-wrapper'>
+              <ReactPlayer
+                  controls={true}
+                  light={'/thumbnail.png'}
+                  className='react-player'
+                  url='https://res.cloudinary.com/afzaal1721/video/upload/v1638188675/Roughneck_Origin_hc4amh.mp4'
+                  width='100%'
+                  height='100%'
+              />
+          </div>
         {/*<div className="overlay">*/}
         {/*  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">*/}
         {/*    <h1 className=" text-4xl md:text-6xl font-quest font-bold text-white text-center  z-50">*/}
