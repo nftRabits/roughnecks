9ed64a07d37e867d3dbae03bdf26e4ad074a3dd9
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-29 09:39:04 +0500

    video section bug fixed

--- views/Video.js
+++ views/Video.js
@@ -7,13 +7,13 @@ const Video = () => {
         <video autoPlay muted id="myVideo">
           <source src="/images/Roughneck Origin.mp4" type="video/mp4" />
         </video>
-        <div className="overlay">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <h1 className=" text-4xl md:text-6xl font-quest font-bold text-white text-center  z-50">
-              VIDEO SECTION PLACEHOLDER
-            </h1>
-          </div>
-        </div>
+        {/*<div className="overlay">*/}
+        {/*  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">*/}
+        {/*    <h1 className=" text-4xl md:text-6xl font-quest font-bold text-white text-center  z-50">*/}
+        {/*      VIDEO SECTION PLACEHOLDER*/}
+        {/*    </h1>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </div>
   );
