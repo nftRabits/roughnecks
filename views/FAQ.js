d5454de0d5001343b4bada82004d4cc8c227f77e
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-29 09:30:42 +0500

    faq's updated

--- views/FAQ.js
+++ views/FAQ.js
@@ -59,34 +59,32 @@ export default FAQ;
 const generalQuestion = [
   {
     id: 1,
-    title: "1. When is the launch? ",
-    description: `We’re planning to launch on December 4th, 2021.`
+    title: "1. When is the main mint? ",
+    description: `11th January 2022`
   },
   {
     id: 2,
     title: "2. How much will minting cost?",
-    description: `Mint price will be 1.75 SOL and there will be 9,999 Roaming Cubs available to mint.`
+    description: `Mint price will be .05 ETH and there will be 10000 Roughneck Rabbits available to mint.`
   },
   {
     id: 3,
-    title: "3. Is there a mint limit?",
-    description: `Yes and no. We will be limiting people to 1 Roaming Cub per transaction. This does mean you can return to the mint section as many times as you want, though.`
+    title: "3. Is there A whitelist? How do I get on it?",
+    description: `Yes. Join the Roughneck Rabbit discord for full details on the white list opportunities.`
   },
   {
     id: 4,
-    title: "4. How to mint?",
-    description: `In order to mint, you will have to wait till our launch day and you need to have a Solana Chain supporting wallet. We ultimately recommend using Phantom, but feel free to use your favorite.
-                    You will need to fund your wallet, connect said wallet to page, click mint, approve and wait to receive your Roaming Cub!
-                     Every cub has a story, what’s yours?`
+    title: "4. Is there a mint limit?",
+    description: `Yes. 3 per person, per transaction.`
   },
   {
     id: 5,
-    title: "5. Where can I trade my Roaming Cubs?",
-    description: `Roaming Cubs will be available for trading at launch on Solanart.io.`
+    title: "5. How to mint?",
+    description: `In order to mint, you will have to wait till our launch day and you need to have a Etheruim supporting wallet. You will need to fund your wallet, connect said wallet to page, click mint, approve and wait to receive your Roughneck Rabbit.`
   },
   {
     id: 6,
     title: "6. Is rarity important to you?",
-    description: `Yes, rarity is very important to us. This is why we’ve designed a Rarity System that lists the traits from “Common”, all the way through “Legendary”. All of Roaming Cubs’ 89 traits and their rarity level can be viewed here. `
+    description: `Yes, rarity is very important to us. This is why we’ve designed a Rarity System that lists the traits from “Common”, all the way through “Legendary”. All of Roughneck Rabbits’ 93 traits and their rarity level can be viewed soon.`
   }
 ];
